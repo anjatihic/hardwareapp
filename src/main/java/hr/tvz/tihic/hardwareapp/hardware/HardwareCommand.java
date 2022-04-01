41c715be6b192c232da40934d65d17c9141462be
Anja Tihic <atihic99@gmail.com>
2022-04-01 14:11:36 +0200

    Specify constraints and write error messages

---
+++ src/main/java/hr/tvz/tihic/hardwareapp/hardware/HardwareCommand.java
@@ -0,0 +1,25 @@
+package hr.tvz.tihic.hardwareapp.hardware;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+
+public class HardwareCommand {
+    @NotBlank(message = "Name cannot be empty")
+    private String name;
+
+    @NotBlank(message = "Code cannot be empty")
+    @PositiveOrZero(message = "Code number cannot be negative")
+    private String code;
+
+    @NotBlank(message = "Price cannot be empty")
+    @Positive(message = "Price cannot be zero or below")
+    private double price;
+
+    @NotBlank(message = "Number of available hardware cannot be empty")
+    @PositiveOrZero(message = "Number of available hardware cannot be below zero")
+    private Integer numberAvailable;
+
+    @NotBlank(message = "Hardware type cannot be empty")
+    private Hardware.Type type;
+}
