e69d7041a7ad9e999f6a7886216a442d7acabc8c
Anja Tihic <atihic99@gmail.com>
2022-04-20 19:52:00 +0200

    Add detailed hardware view to a separate url

--- HardwareApp/src/app/detailed-hardware/detailed-hardware.component.ts
+++ HardwareApp/src/app/detailed-hardware/detailed-hardware.component.ts
@@ -1,8 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hardware } from '../models/hardware';
 import {ActivatedRoute} from "@angular/router";
 import {HardwareServiceService} from "../services/hardware-service.service";
-import {filter, find, first, map, Observable} from "rxjs";
 
 @Component({
   selector: 'app-detailed-hardware',
@@ -10,21 +9,21 @@ import {filter, find, first, map, Observable} from "rxjs";
   styleUrls: ['./detailed-hardware.component.css']
 })
 export class DetailedHardwareComponent implements OnInit {
-
   public hardware!: Hardware;
   public code!: string | null;
+
 
   constructor(private route: ActivatedRoute, private hardwareService: HardwareServiceService) {}
 
   ngOnInit(): void {
     this.code = this.route.snapshot.paramMap.get('code');
-    const codeNew = <string> this.code;
-    console.log(this.hardwareService.getAllHardware());
-    const foundHardware= this.hardwareService.getHardwareByCode(codeNew);
+    this.getHardwareByCode();
 
-    console.log("Typeof " + typeof (foundHardware));
-    console.log(this.hardware);
-    console.log(this.hardware.name);
+  }
+
+  getHardwareByCode(): void{
+    this.hardwareService.getHardwareByCode(<string>this.code)
+      .subscribe(value => this.hardware = value);
   }
 
 
