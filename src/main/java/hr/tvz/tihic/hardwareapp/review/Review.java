c3a982eef653787aa32bf4ed3833f79e43f62bd1
Anja Tihic <atihic99@gmail.com>
2022-05-11 13:46:12 +0200

    Trying

--- src/main/java/hr/tvz/tihic/hardwareapp/review/Review.java
+++ src/main/java/hr/tvz/tihic/hardwareapp/review/Review.java
@@ -1,8 +1,10 @@
 package hr.tvz.tihic.hardwareapp.review;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Review {
 
     private long id;
@@ -12,12 +14,5 @@ public class Review {
 
     public enum Rating{
         ONE, TWO, THREE, FOUR, FIVE
-    }
-
-    public Review(long id, String title, String reviewText, Rating rating) {
-        this.id = id;
-        this.title = title;
-        this.reviewText = reviewText;
-        this.rating = rating;
     }
 }
