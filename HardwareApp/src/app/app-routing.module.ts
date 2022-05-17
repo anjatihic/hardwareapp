07815da0f6eb2fdb9672233eb8c9b6b073d8929c
Anja Tihic <atihic99@gmail.com>
2022-05-17 14:22:20 +0200

    Change app routes

--- HardwareApp/src/app/app-routing.module.ts
+++ HardwareApp/src/app/app-routing.module.ts
@@ -2,12 +2,49 @@ import { NgModule } from '@angular/core';
 import {RouterModule, Routes} from "@angular/router";
 import {AllHardwareComponent} from "./all-hardware/all-hardware.component";
 import {DetailedHardwareComponent} from "./detailed-hardware/detailed-hardware.component";
-import {FindReviewComponent} from "./find-review/find-review.component";
+import {LoginComponent} from "./login/login.component";
+import {LoggedInGuard} from "./security/logged-in.guard";
+import {AdminAuthorityGuard} from "./security/admin-authority.guard";
+import {ForbiddenPageComponent} from "./forbidden-page/forbidden-page.component";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
 const routes: Routes = [
-  {path: 'hardware', component: AllHardwareComponent},
-  {path: 'detail/:code', component: DetailedHardwareComponent},
-  {path: 'findreview', component: FindReviewComponent}
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'hardware',
+    component: AllHardwareComponent,
+    canActivate: [LoggedInGuard]
+  },
+  {
+    path: 'detail/:code',
+    component: DetailedHardwareComponent,
+    canActivate: [LoggedInGuard]
+  },
+  {
+    path: 'new-hardware',
+    component: NewHardwareComponent,
+    canActivate: [AdminAuthorityGuard]
+  },
+  {
+    path: 'forbidden',
+    component: ForbiddenPageComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
 ];
 
 @NgModule({
